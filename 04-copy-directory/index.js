13d3cf474431cd2a26d8c8a6cb325cffd5711fb4
Kate Mashko <katika.life@gmail.com>
2024-01-20 10:13:30 +0300

    feat: add function copyDir

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const pathFolder = path.resolve(__dirname, 'files');
+const pathFolderCopy = path.resolve(__dirname, 'files-copy');
+
+function copyDir() {
+  fs.mkdir(pathFolderCopy, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(pathFolder, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.log('Error!' + err);
+    } else {
+      files.forEach((file) => {
+        let filePath = path.resolve(pathFolder, file.name);
+        let fileCopyPath = path.resolve(pathFolderCopy, file.name);
+
+        fs.copyFile(filePath, fileCopyPath, () => {});
+      });
+    }
+  });
+}
+copyDir();
